2fb89049e3390bc70ccaa5c5e2444b82105a10e4
Mdlalose4life <blessing1059@gmail.com>
2022-12-14 13:53:29 +0200

    Project Push

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	
+}
