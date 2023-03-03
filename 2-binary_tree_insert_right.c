c85c76524d62c2a35b8a781aeaea00d5c3befbf3
Mdlalose4life <blessing1059@gmail.com>
2023-03-04 00:28:37 +0200

    Edditions

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,33 +1,50 @@
+
+
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
- * binary_tree_insert_left - Function that inserts a node to the left.
- * @parent: pointer to the node that inserts the right-child in.
- * @value: value to store in the new node.
- * Return: Pointer to a new node. return NULL if no parent 
+ * binary_tree_node_2 - creates a binary tree node
+ * @parent: parnt of the node to create
+ * @value: value to store in new node
+ *
+ * Return: pointer to the new node
+ */
+binary_tree_t *binary_tree_node_2(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = new_node->right = NULL;
+	return (new_node);
+}
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ *
+ * Description: If parent already has a right-child, the new node must take its
+ * place and the old right-child must be set as the right-child of the new node
+ *
+ * Return: pointer to the created node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *r_node = NULL;
+	binary_tree_t *right_child;
 
 	if (parent == NULL)
 		return (NULL);
-	r_node = malloc(sizeof(binary_tree_t));
-	if (r_node == NULL)
+	right_child = binary_tree_node_2(parent, value);
+	if (right_child == NULL)
 		return (NULL);
-	r_node->n = value;
-	r_node->parent = parent;
-	r_node->left = NULL;
-	if (parent->right != NULL)
-	{
-		parent->right->parent = r_node;
-		r_node->right = parent->right;
-	}
-	else
-	{
-		parent->right = r_node;
-		r_node->right = NULL;
-	}
-	parent->right = r_node;
-	return (r_node);
+	right_child->right = parent->right;
+	if (right_child->right != NULL)
+		right_child->right->parent = right_child;
+	parent->right = right_child;
+	return (right_child);
 }
